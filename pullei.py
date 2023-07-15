fde183a70497dd1de197e6e558d84096381a0095
Rafa <73889216+prnig@users.noreply.github.com>
2023-07-15 12:35:42 +0300

    Update pullei.py
    Now, downloaded files are not only segregated between 'CVEs' and 'other' folders but also organized into subfolders named by the date the pull request was created on GitHub. This allows for easier tracking of pull requests by their creation date, improving the organization of downloaded files.

--- pullei.py
+++ pullei.py
@@ -3,6 +3,7 @@ import argparse
 import os
 from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import unquote
+from datetime import datetime
 
 
 def fetch_pull_requests(github_token):
@@ -22,7 +23,8 @@ def fetch_pull_requests(github_token):
             break
 
         for pull_request in prs_data:
-            pull_requests.append(pull_request['number'])
+            pr_date = datetime.strptime(pull_request['created_at'], '%Y-%m-%dT%H:%M:%SZ').strftime('%d-%m-%y')
+            pull_requests.append((pull_request['number'], pr_date))
 
         page_number += 1
 
@@ -48,16 +50,16 @@ def file_exists_in_directory(file_name, directory):
     return False
 
 
-def download_file(url, nuclei_templates_dir, cves_only, download_directory):
+def download_file(url, nuclei_templates_dir, cves_only, download_directory, pr_date):
     decoded_url = unquote(url)
     file_name = os.path.basename(decoded_url)
-    
+
     download_path = os.path.join(download_directory, "pullei")
     if "cves" in url:
-        download_path = os.path.join(download_path, "cves")
+        download_path = os.path.join(download_path, "cves", pr_date)
     else:
-        download_path = os.path.join(download_path, "other")
-    
+        download_path = os.path.join(download_path, "other", pr_date)
+
     if not os.path.exists(download_path):
         os.makedirs(download_path)
 
@@ -67,7 +69,7 @@ def download_file(url, nuclei_templates_dir, cves_only, download_directory):
     if file_exists_in_directory(file_name, nuclei_templates_dir) or file_exists_in_directory(file_name, download_path):
         print(f"\033[93mFile {file_name} already exists. Skipping download.\033[0m")
         return
-    
+
     file_path = os.path.join(download_path, file_name)
     response = requests.get(url)
     with open(file_path, 'wb') as f:
@@ -85,10 +87,10 @@ def execute_script():
 
     pull_request_numbers = fetch_pull_requests(script_args.github_token)
 
-    for pull_request_number in pull_request_numbers:
+    for pull_request_number, pr_date in pull_request_numbers:
         file_urls = fetch_files(pull_request_number, script_args.github_token)
         for url in file_urls:
-            download_file(url, script_args.nuclei_templates_path, script_args.cves_only, script_args.download_directory)
+            download_file(url, script_args.nuclei_templates_path, script_args.cves_only, script_args.download_directory, pr_date)
 
 
 if __name__ == "__main__":
